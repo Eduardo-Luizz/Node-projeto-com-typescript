5e35cfd2f61ddc50f9d0fb6be02e5fcc559a32ca
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-14 10:23:56 -0300

    Chamando a rota direto pelo vscode sem precisar do swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { createCourse } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello!" });
-})
+// app.get("/", (request, response) => {
+//   return response.json({ message: "Hello!" });
+// })
+
+app.get("/", createCourse); // Chamar rota sem ser pelo insomnia
 
 app.listen(3333);
