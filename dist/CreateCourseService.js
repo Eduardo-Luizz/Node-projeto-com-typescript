6c7d768281af6e92775fe7ce7a7cc86f3f3c2877
Eduardo-Luizz <91149760+Eduardo-Luizz@users.noreply.github.com>
2022-07-14 10:24:46 -0300

    Arquivos de compilação criados pelo typescript que vão ser executados pelo node

---
+++ dist/CreateCourseService.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * name - string
+ * duration - number
+ * educator - string
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+class CreateCourseService {
+    // execute(data: Course) {
+    //   console.log(data.name, data.duration, data.educator);
+    // }
+    // Outra forma de fazer
+    execute({ duration, educator, name }) {
+        console.log(name, duration, educator);
+    }
+}
+exports.default = new CreateCourseService();
